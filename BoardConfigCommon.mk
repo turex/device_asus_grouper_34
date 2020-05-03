7037ab9528a49b3a6668105e99369e3e592419b8
turex <salvo95ct@gmail.com>
2020-05-03 18:23:28 +0200

    first commit

---
+++ BoardConfigCommon.mk
@@ -0,0 +1,70 @@
+# Copyright (C) 2010 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+include hardware/nvidia/tegra3/BoardConfigCommon.mk
+
+TARGET_BOOTLOADER_BOARD_NAME := grouper
+
+# Filesystem
+TARGET_USERIMAGES_USE_EXT4 := true
+ifeq ($(HOST_OS),linux)
+TARGET_USERIMAGES_USE_F2FS := true
+endif
+BOARD_BOOTIMAGE_PARTITION_SIZE := 8388608
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 12582912
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 681574400
+BOARD_CACHEIMAGE_PARTITION_SIZE := 464519168
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 6567231488
+BOARD_FLASH_BLOCK_SIZE := 4096
+
+# Disable journaling on system.img to save space
+BOARD_SYSTEMIMAGE_JOURNAL_SIZE := 0
+
+# Only pre-optimize the boot image
+WITH_DEXPREOPT_BOOT_IMG_ONLY := true
+
+# Configure jemalloc for low-memory
+MALLOC_SVELTE := true
+
+# Use clang platform builds
+USE_CLANG_PLATFORM_BUILD := true
+
+# Kernel
+BOARD_KERNEL_CMDLINE := androidboot.hardware=$(TARGET_BOOTLOADER_BOARD_NAME)
+TARGET_KERNEL_CONFIG := grouper_defconfig
+TARGET_KERNEL_SOURCE := kernel/nvidia/tegra3
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/linaro_4.9/bin
+KERNEL_TOOLCHAIN_PREFIX := arm-eabi-
+
+# Wi-Fi
+BOARD_WLAN_DEVICE           := bcmdhd
+WPA_SUPPLICANT_VERSION      := VER_0_8_X
+BOARD_WPA_SUPPLICANT_DRIVER := NL80211
+BOARD_HOSTAPD_DRIVER        := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB   := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+WIFI_DRIVER_FW_PATH_PARAM   := "/sys/module/bcmdhd/parameters/firmware_path"
+WIFI_DRIVER_FW_PATH_STA     := "/vendor/firmware/fw_bcmdhd.bin"
+WIFI_DRIVER_FW_PATH_AP      := "/vendor/firmware/fw_bcmdhd_apsta.bin"
+
+# Bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_BCM := true
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/asus/grouper/bluetooth
+
+# Dump State
+BOARD_HAL_STATIC_LIBRARIES := libdumpstate.grouper
+
+# Security
+BOARD_SEPOLICY_DIRS += device/asus/grouper/sepolicy
---
+++ device-common.mk
@@ -0,0 +1,126 @@
+#
+# Copyright (C) 2010 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+$(call inherit-product, hardware/nvidia/tegra3/tegra3.mk)
+
+PRODUCT_CHARACTERISTICS := tablet,nosdcard
+
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := hdpi
+
+# Dalvik VM config
+$(call inherit-product, frameworks/native/build/tablet-7in-hdpi-1024-dalvik-heap.mk)
+
+# Init files
+PRODUCT_COPY_FILES += \
+    device/asus/grouper/rootdir/init.grouper.usb.rc:root/init.grouper.usb.rc \
+    device/asus/grouper/rootdir/ueventd.grouper.rc:root/ueventd.grouper.rc
+
+# These are the hardware-specific features
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/tablet_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml \
+    frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
+    frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
+    frameworks/native/data/etc/android.hardware.ethernet.xml:system/etc/permissions/android.hardware.ethernet.xml \
+    frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+    frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
+    frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
+    frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
+    frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+    frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
+    frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
+    frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
+
+# File that declares the MIFARE NFC constant
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/com.nxp.mifare.xml:system/etc/permissions/com.nxp.mifare.xml
+
+# Input device configs
+PRODUCT_COPY_FILES += \
+    device/asus/grouper/touchscreen/elan-touchscreen.idc:system/usr/idc/elan-touchscreen.idc \
+    device/asus/grouper/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl
+
+# GPS
+PRODUCT_COPY_FILES += \
+    device/asus/grouper/gps/gps.conf:system/etc/gps.conf \
+    device/asus/grouper/gps/gps.xml:system/etc/gps.xml
+
+PRODUCT_PACKAGES += \
+    libgpsd-compat \
+    libstlport
+
+# Wi-Fi
+PRODUCT_PACKAGES += \
+    libwpa_client \
+    hostapd \
+    dhcpcd.conf \
+    wpa_supplicant \
+    wpa_supplicant.conf
+
+WIFI_BAND := 802_11_BG
+$(call inherit-product-if-exists, hardware/broadcom/wlan/bcmdhd/firmware/bcm4330/device-bcm.mk)
+
+# HALs
+PRODUCT_PACKAGES += \
+    audio.primary.grouper \
+    lights.grouper
+
+# Sensors
+PRODUCT_COPY_FILES += \
+    device/asus/grouper/sensors/sensors-load-calibration.sh:system/bin/sensors-load-calibration.sh
+
+# Audio
+PRODUCT_PACKAGES += \
+    audio.a2dp.default \
+    audio.usb.default \
+    audio.r_submix.default
+
+PRODUCT_COPY_FILES += \
+    device/asus/grouper/audio/audio_policy.conf:system/etc/audio_policy.conf \
+    device/asus/grouper/audio/mixer_paths.xml:system/etc/mixer_paths.xml
+
+# NFC
+PRODUCT_PACKAGES += \
+    nfc.grouper \
+    libpn544_fw \
+    NfcNxp \
+    Tag
+
+# Filesystem management tools
+PRODUCT_PACKAGES += \
+    fsck.f2fs \
+    mkfs.f2fs
+
+# Shell
+ifneq ($(filter eng userdebug,$(TARGET_BUILD_VARIANT)),)
+PRODUCT_PACKAGES += \
+    Terminal
+endif
+
+# Media
+PRODUCT_COPY_FILES += \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
+    device/asus/grouper/media/media_profiles.xml:system/etc/media_profiles.xml \
+    device/asus/grouper/media/media_codecs.xml:system/etc/media_codecs.xml
+
+# Vendor blobs
+$(call inherit-product, vendor/asus/grouper/asus-vendor.mk)
+$(call inherit-product, vendor/broadcom/grouper/broadcom-vendor.mk)
+$(call inherit-product, vendor/invensense/grouper/invensense-vendor.mk)
